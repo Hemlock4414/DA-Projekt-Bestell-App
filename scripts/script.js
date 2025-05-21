2aebbf3ad6da017c58c662ad5bb675cbcf43fac5
Hemlock4414 <antonio.marrara@gmx.ch>
2025-05-21 12:16:39 +0200

    restaurant-wrapper done

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
-// Funktion zur Initialisierung der gesamten Speisekarte
+function renderCategory(dishesArray, elementId) {
+    let container = document.getElementById(elementId);
+    container.innerHTML = "";
+
+    for (let i = 0; i < dishesArray.length; i++) {
+        let dish = dishesArray[i];
+        container.innerHTML += getMenuTemplate(dish);
+    }
+}
+
 function init() {
-    renderSandwiches();
-    renderSweets();
-    renderCoffee();
-    renderShakes();
+    renderCategory(dishesData.sandwiches, 'content-sandwiches');
+    renderCategory(dishesData.sweets, 'content-sweets');
+    renderCategory(dishesData.coffee, 'content-coffee');
+    renderCategory(dishesData.shakes, 'content-shakes');
 }
 
 // Hilfsfunktion zur Formatierung der Preise (z.B. 9.40 -> 9,40 €)
@@ -11,42 +20,51 @@ function formatPrice(price) {
     return price.toFixed(2).replace('.', ',') + ' €';
 }
 
-// Funktion zum Rendern der Sandwiches mit For-Schleife
-function renderSandwiches() {
-    let menuSandwiches = document.getElementById('content-sandwiches');
-    menuSandwiches.innerHTML = "";
-    
-    for (let i = 0; i < dishesSandwiches.length; i++) {
-        menuSandwiches.innerHTML += getSandwichTemplate(i);
-    }
-}
 
-// Funktion zum Rendern der Süßspeisen mit For-Schleife
-function renderSweets() {
-    let menuSweets = document.getElementById('content-sweets');
-    menuSweets.innerHTML = "";
-    
-    for (let i = 0; i < dishesSweets.length; i++) {
-        menuSweets.innerHTML += getSweetsTemplate(i);
-    }
-}
+// // Funktion zur Initialisierung der gesamten Speisekarte
+// function init() {
+//     renderSandwiches();
+//     renderSweets();
+//     renderCoffee();
+//     renderShakes();
+// }
 
-// Funktion zum Rendern der Kaffee-Einträge mit For-Schleife
-function renderCoffee() {
-    let menuCoffee = document.getElementById('content-coffee');
-    menuCoffee.innerHTML = "";
+// // Funktion zum Rendern der Sandwiches mit For-Schleife
+// function renderSandwiches() {
+//     let menuSandwiches = document.getElementById('content-sandwiches');
+//     menuSandwiches.innerHTML = "";
     
-    for (let i = 0; i < dishesCoffee.length; i++) {
-        menuCoffee.innerHTML += getCoffeeTemplate(i);
-    }
-}
+//     for (let i = 0; i < dishesSandwiches.length; i++) {
+//         menuSandwiches.innerHTML += getSandwichTemplate(i);
+//     }
+// }
 
-// Funktion zum Rendern der Shakes mit For-Schleife
-function renderShakes() {
-    let menuShakes = document.getElementById('content-shakes');
-    menuShakes.innerHTML = "";
+// // Funktion zum Rendern der Süßspeisen mit For-Schleife
+// function renderSweets() {
+//     let menuSweets = document.getElementById('content-sweets');
+//     menuSweets.innerHTML = "";
     
-    for (let i = 0; i < dishesShakes.length; i++) {
-        menuShakes.innerHTML += getShakesTemplate(i);
-    }
-}
+//     for (let i = 0; i < dishesSweets.length; i++) {
+//         menuSweets.innerHTML += getSweetsTemplate(i);
+//     }
+// }
+
+// // Funktion zum Rendern der Kaffee-Einträge mit For-Schleife
+// function renderCoffee() {
+//     let menuCoffee = document.getElementById('content-coffee');
+//     menuCoffee.innerHTML = "";
+    
+//     for (let i = 0; i < dishesCoffee.length; i++) {
+//         menuCoffee.innerHTML += getCoffeeTemplate(i);
+//     }
+// }
+
+// // Funktion zum Rendern der Shakes mit For-Schleife
+// function renderShakes() {
+//     let menuShakes = document.getElementById('content-shakes');
+//     menuShakes.innerHTML = "";
+    
+//     for (let i = 0; i < dishesShakes.length; i++) {
+//         menuShakes.innerHTML += getShakesTemplate(i);
+//     }
+// }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,55 +1,72 @@
-// Template für Sandwich-Einträge
-function getSandwichTemplate(i) {
+function getMenuTemplate(dish) {
     return `
         <div class="menu-item">
             <div class="menu-item-left">
-                <h3 class="menu-item-name">${dishesSandwiches[i].name}</h3>
-                <p class="menu-item-description">${dishesSandwiches[i].description}</p>
-                <p class="menu-item-price">${formatPrice(dishesSandwiches[i].price)}</p>
+                <h3 class="menu-item-name">${dish.name}</h3>
+                <p class="menu-item-description">${dish.description}</p>
+                <p class="menu-item-price">${formatPrice(dish.price)}</p>
             </div>
-            <div class="menu-item-right"></div>
+            <div class="menu-item-right">
+                <span class="plus-icon">+</span>
+            </div>
         </div>
     `;
 }
 
-// Template für Süßspeisen-Einträge
-function getSweetsTemplate(i) {
-    return `
-        <div class="menu-item">
-            <div class="menu-item-left">
-                <h3 class="menu-item-name">${dishesSweets[i].name}</h3>            
-                <p class="menu-item-description">${dishesSweets[i].description}</p>
-                <p class="menu-item-price">${formatPrice(dishesSweets[i].price)}</p>
-            </div>
-            <div class="menu-item-right"></div>
-        </div>
-    `;
-}
 
-// Template für Kaffee-Einträge
-function getCoffeeTemplate(i) {
-    return `
-        <div class="menu-item">
-            <div class="menu-item-left">
-                <h3 class="menu-item-name">${dishesCoffee[i].name}</h3>
-                <p class="menu-item-description">${dishesCoffee[i].description}</p>
-                <p class="menu-item-price">${formatPrice(dishesCoffee[i].price)}</p>
-            </div>
-            <div class="menu-item-right"></div>
-        </div>
-    `;
-}
+// // Template für Sandwich-Einträge
+// function getSandwichTemplate(dataArray[i]) {
+//     return `
+//         <div class="menu-item">
+//             <div class="menu-item-left">
+//                 <h2 class="menu-item-title">${dataArray[i].category}<h2>
+//                 <h3 class="menu-item-name">${dataArray[i].name}</h3>
+//                 <p class="menu-item-description">${dataArray[i].description}</p>
+//                 <p class="menu-item-price">${formatPrice(dataArray[i].price)}</p>
+//             </div>
+//             <div class="menu-item-right"></div>
+//         </div>
+//     `;
+// }
 
-// Template für Shakes-Einträge
-function getShakesTemplate(i) {
-    return `
-        <div class="menu-item">
-            <div class="menu-item-left">
-                <h3 class="menu-item-name">${dishesShakes[i].name}</h3>
-                <p class="menu-item-description">${dishesShakes[i].description}</p>
-                <p class="menu-item-price">${formatPrice(dishesShakes[i].price)}</p>
-            </div>
-            <div class="menu-item-right"></div>
-        </div>
-    `;
-}
+// // Template für Süßspeisen-Einträge
+// function getSweetsTemplate(dataArray[i]) {
+//     return `
+//         <div class="menu-item">
+//             <div class="menu-item-left">
+//                 <h3 class="menu-item-name">${dataArray[i].name}</h3>            
+//                 <p class="menu-item-description">${dataArray[i].description}</p>
+//                 <p class="menu-item-price">${formatPrice(dataArray[i].price)}</p>
+//             </div>
+//             <div class="menu-item-right"></div>
+//         </div>
+//     `;
+// }
+
+// // Template für Kaffee-Einträge
+// function getCoffeeTemplate(dataArray[i]) {
+//     return `
+//         <div class="menu-item">
+//             <div class="menu-item-left">
+//                 <h3 class="menu-item-name">${dataArray[i].name}</h3>
+//                 <p class="menu-item-description">${dataArray[i].description}</p>
+//                 <p class="menu-item-price">${formatPrice(dataArray[i].price)}</p>
+//             </div>
+//             <div class="menu-item-right"></div>
+//         </div>
+//     `;
+// }
+
+// // Template für Shakes-Einträge
+// function getShakesTemplate(dataArray[i]) {
+//     return `
+//         <div class="menu-item">
+//             <div class="menu-item-left">
+//                 <h3 class="menu-item-name">${dataArray[i].name}</h3>
+//                 <p class="menu-item-description">${dataArray[i].description}</p>
+//                 <p class="menu-item-price">${formatPrice(dataArray[i].price)}</p>
+//             </div>
+//             <div class="menu-item-right"></div>
+//         </div>
+//     `;
+// }
